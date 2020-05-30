8e3ad6ebc0567226c780097fc3f3b3b38d464844
will-ing <willkoger@gmail.com>
2020-05-30 13:24:15 -0700

    create branch

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,8 @@
+
+
+print('**************************************')
+print('** Welcome to the Snakes Cafe! **')
+print('** Please see our menu below. **')
+print('**')
+print('** To quit at any time, type "quit" **')
+print('**************************************')
